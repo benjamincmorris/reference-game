315dea5b396a5799880c771466413065cf63b79f
CaLLab <benmorris@uchicago.edu>
2017-03-10 13:30:47 -0600

    final edits for 3.10 piloting

--- exp/reference_sender.js
+++ exp/reference_sender.js
@@ -343,7 +343,7 @@ try {
     var xmlHttp = null;
     var filename = "sender_condition_counts";
     // var condCounts = "1,1;2,1;3,1;4,1;5,1;6,1;7,1;8,1;9,1;10,1;11,1;12,1;13,1;14,1;15,1;16,1;17,1;18,1;19,1;20,1;21,1;22,1;23,1;24,1;25,1;26,1;27,1;28,1;29,1;30,1;31,1;32,1;33,1;34,1;35,1;36,1;37,1;38,1;39,1;40,1;41,1;42,1;43,1;44,1;45,1;46,1;47,1;48,1;49,1;50,1;51,1;52,1;53,1;54,1;55,1;56,1;57,1;58,1;59,1;60,1";
-    var condCounts = "11,1;12,1;13,1;14,1;15,1;16,1;17,1;18,1;19,1;20,1;21,1;22,1;23,1;24,1;25,1;26,1;27,1;28,1;29,1;30,1;41,1;42,1;43,1;44,1;45,1;46,1;47,1;48,1;49,1;50,1;51,1;52,1;53,1;54,1;55,1;56,1;57,1;58,1;59,1;60,1";
+    var condCounts = "1,1;2,1;3,1;4,1;5,1;6,1;7,1;8,1;9,1;10,1;11,1;12,1;13,1;14,1;15,1;16,1;17,1;18,1;19,1;20,1";
 
     // var condCounts = "1,0;2,0"
     // var condCounts= "100_30,30;80_50,30"
@@ -365,14 +365,14 @@ try {
 
                   do_all_the_setup()
                   // for debugging, use line below to jump around the exp
-                        // experiment.game(0,0,30);
+                        // experiment.prestudy(0,0,30);
                 }
 
         }
     };
     if (turk.workerId.length > 0) { 
         //if we are on turk, send turker parameter to the php
-        console.log("decrementing the count...")
+        console.log("time stamping...")
         // xmlHttp.open("GET", "https://callab.uchicago.edu/experiments/reference/php/_crementer.php?filename=" + filename + "&to_decrement=" + cond, true);
           xmlHttp.open("GET", "https://callab.uchicago.edu/experiments/reference/php/yoking_init.php?filename=" + filename + "&conds=" + condCounts + "&turkID=true", true);
     } else {
@@ -384,7 +384,7 @@ try {
 
 } catch (e) {
   // this catch condition needs updating
-    var condition = "100_30";
+    var subjectIdentifer = "waiting";
 }
 
 
@@ -407,14 +407,14 @@ var totalSlides = 1 + 1 + exposureStimuli(imgArray).length + 1 + imgArray.length
 
 function do_all_the_setup() {
 
-                if(subjectIdentifier <= 30) {
+                if(subjectIdentifier <= 10) {
                   cond = "100_30"
                   partnersExposure = "0"
-                  if (subjectIdentifier <= 20) {
-                    partnersExposure ="1/2"
-                  } if (subjectIdentifier<= 10) {
-                    partnersExposure = "1"
-                  }
+                  // if (subjectIdentifier <= 20) {
+                  //   partnersExposure ="1/2"
+                  // } if (subjectIdentifier<= 10) {
+                  //   partnersExposure = "1"
+                  // }
                 } else {cond="80_50";
                         trueLabelPoints = 80;
                         trueClickPoints = 50;
@@ -423,11 +423,11 @@ function do_all_the_setup() {
                         pointsClickWrong = -50;
                         pointsBothWrong = -70;
                   partnersExposure = "0"
-                  if (subjectIdentifier <= 50) {
-                    partnersExposure ="1/2"
-                  } if (subjectIdentifier <= 40) {
-                    partnersExposure = "1"
-                  }
+                  // if (subjectIdentifier <= 50) {
+                  //   partnersExposure ="1/2"
+                  // } if (subjectIdentifier <= 40) {
+                  //   partnersExposure = "1"
+                  // }
                 }
 
 
@@ -844,7 +844,7 @@ var experiment = {
             setTimeout(function() {document.getElementById("chairDistractor").style.border= ""}, 500)
             //bring in the new
             setTimeout(function() {$("#clickText").fadeIn(500)}, 500)
-            setTimeout(function() {document.getElementById("clickText").innerHTML="<br> You can also send a message by clicking the object here... <br> <strong> Give it a try </strong>"
+            setTimeout(function() {document.getElementById("clickText").innerHTML="<br> You can also send a message by clicking the object here... <br> <strong> Try clicking on the shoe </strong>"
                 }, 2000)
             //diy demonstration
             //check if an element in the array is clicked on
